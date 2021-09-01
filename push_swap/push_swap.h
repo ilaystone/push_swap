2832dee9104df44d2d87800dbd5126eb4225cf49
Ilyass Khadem <ikhadem@e2r4p7.1337.ma>
2021-09-01 11:30:06 +0100

    added add function to t_stack lib

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 14:59:06 by ikhadem           #+#    #+#             */
-/*   Updated: 2021/09/01 10:31:04 by ikhadem          ###   ########.fr       */
+/*   Updated: 2021/09/01 11:23:07 by ikhadem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,27 @@
 # include "ft_utils.h"
 # include <stdio.h>
 
-/*
-*	macros
-*/
+typedef enum e_push_swap_moves
+{
+	e_sa,
+	e_sb,
+	e_ss,
+	e_ra,
+	e_rb,
+	e_rr,
+	e_rra,
+	e_rrb,
+	e_rrr,
+	e_pa,
+	e_pb,
+}				t_push_swap_moves;
 
 # define P_MAX_INT 2147483647
 # define P_MIN_INT -2147483648
+
+/***************
+**** protos ****
+***************/
 
 t_stack		*set_stack(int , char **);
 int			push_swap_check_errors(int , char **);
@@ -31,8 +46,7 @@ int			check_args_within_limits(int , char **);
 int			cheack_args_duplicates(int , char **);
 void		push_swap_sort(int , char **);
 void		push_swap_sequential_sort(t_stack **, t_stack **);
-void		check_stack_a(size_t size, int bit, t_stack **a, t_stack **b);
-void		check_stack_b(size_t size, int bit, t_stack **a, t_stack **b);
+void		push_swap_sort_three(t_stack **a, t_stack **res);
 
 /********************
 **** OPERATIONS ****
--- push_swap/push_swap_sort_three.c
+++ push_swap/push_swap_sort_three.c
@@ -1,23 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap_sequential_sort.c                        :+:      :+:    :+:   */
+/*   push_swap_sort_three.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/30 10:17:05 by ikhadem           #+#    #+#             */
-/*   Updated: 2021/09/01 11:28:05 by ikhadem          ###   ########.fr       */
+/*   Created: 2021/09/01 10:43:13 by ikhadem           #+#    #+#             */
+/*   Updated: 2021/09/01 11:29:43 by ikhadem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void push_swap_sequential_sort(t_stack **a, t_stack **b)
+void	push_swap_sort_three(t_stack **a, t_stack **res)
 {
-	t_stack		*res;
+	int		max;
+	int		min;
 
-	(void)b;
-	t_stack_init(&res);
-	push_swap_sort_three(a, &res);
-	t_stack_display(res);
+	max = t_stack_max(*a);
+	min = t_stack_min(*a);
+	if (t_stack_index(*a, 0) == max && t_stack_index(*a, 1) != min)
+		t_stack_add(res, (int)e_sa);
+	else if (t_stack_index(*a, 2) == max && t_stack_index(*a, 0) != min)
+		t_stack_add(res, (int)e_sa);
+	else if (t_stack_index(*a, 1) == max && t_stack_index(*a, 2) != min)
+		t_stack_add(res, (int)e_sa);
 }
