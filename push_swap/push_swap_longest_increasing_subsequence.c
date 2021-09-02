eec2b5f006bad321966fb20c90429f89e49bf87c
Ilyass Khadem <ikhadem@e2r1p5.1337.ma>
2021-09-02 14:22:04 +0100

    segfault on creation of increasing sequence

--- push_swap/push_swap_longest_increasing_subsequence.c
+++ push_swap/push_swap_longest_increasing_subsequence.c
@@ -6,7 +6,7 @@
 /*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 14:03:47 by ikhadem           #+#    #+#             */
-/*   Updated: 2021/09/02 13:53:57 by ikhadem          ###   ########.fr       */
+/*   Updated: 2021/09/02 14:21:12 by ikhadem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ static int	*create_is_arr(size_t size)
 	return (is_arr);
 }
 
-static void	lis(t_stack **copy)
+static int	*lis(t_stack **copy)
 {
 	int			*is_arr;
 	size_t		size;
@@ -57,23 +57,54 @@ static void	lis(t_stack **copy)
 		while (j < i)
 		{
 			if (t_stack_index(*copy, j) < t_stack_index(*copy, i) &&
-				is_arr[j] >= is_arr[i])
+				is_arr[i] <= is_arr[j])
 				is_arr[i] = 1 + is_arr[j];
 			j++;
 		}
 		i++;
 	}
-	t_stack_display(*copy);
-	for (i =0; i < size; i++)
-		printf("%d\n", is_arr[i]);
+	return (is_arr);
+}
+
+static t_stack	*create_list_of_elements(t_stack *copy, int *lis, size_t size)
+{
+	t_stack		*res;
+	size_t		i;
+	int			max;
+
+	t_stack_init(&res);
+	i = 0;
+	max = lis[0];
+	while (i < size)
+	{
+		if (lis[i] > max)
+			max= lis[i];
+		i++;
+	}
+	i = size - 1;
+	while (i >= 0)
+	{
+		if (lis[i] == max &&
+			(t_stack_size(res) == 0 ||
+			t_stack_index(copy, i) < t_stack_index(res, 0)))
+			{
+				t_stack_push(&res, t_stack_index(copy, i));
+				max--;
+			}
+		i--;
+	}
+	return (res);
 }
 
 t_stack	*push_swap_longest_increasing_subsequence(t_stack **a)
 {
 	t_stack		*copy;
+	t_stack		*lov;
 
 	copy = t_stack_copy(*a);
+	t_stack_display(copy);
 	rotate_smallest_To_top(&copy);
-	lis(&copy);
+	lov = create_list_of_elements(copy, lis(&copy), t_stack_size(copy));
+	t_stack_display(lov);
 	return (copy);
 }
