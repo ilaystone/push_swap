10ad9eae5ac217eb43b7435424ea68d1b2ec7c57
Ilyass Khadem <ikhadem@e1r2p4.1337.ma>
2021-09-20 08:12:23 +0100

    mvp

--- push_swap_exec/ps_push.c
+++ push_swap_exec/ps_push.c
@@ -6,7 +6,7 @@
 /*   By: ikhadem <ikhadem@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/29 13:32:49 by ikhadem           #+#    #+#             */
-/*   Updated: 2021/09/20 08:06:02 by ikhadem          ###   ########.fr       */
+/*   Updated: 2021/09/20 08:08:58 by ikhadem          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	push_stack(t_stack **s1, t_stack **s2)
 {
 	t_stack		*temp;
 
-	if (t_stack_size(s1) > 0)
+	if (t_stack_size(*s1) > 0)
 	{
 		temp = t_stack_pop(s1);
 		t_stack_push(s2, temp->data);
